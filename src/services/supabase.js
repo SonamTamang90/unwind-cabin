422ff93abf50ff556430d4a3d4092ce0b9eb28fc
stamang <stamang.mrbhutan90@gmail.com>
2025-01-23 09:02:04 +0800

    Added supabase connection and react query for fetching cabins

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://rlxuthrcdpvjjbehsfxb.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJseHV0aHJjZHB2ampiZWhzZnhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc1NDg5MzYsImV4cCI6MjA1MzEyNDkzNn0.FYtm8MRoybOBtZTHcoz3wFR2P-7La7Xgzbxfyh-TPXs";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
